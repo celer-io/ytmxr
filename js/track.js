a257144cecf9b0971c323cf80d56527eb785dc27
Clement Lepissier <clem.lepissier@gmail.com>
2018-03-23 07:07:40 +0100

    add volume and playback features

--- js/track.js
+++ js/track.js
@@ -5,7 +5,9 @@ const init = (videoId, key) => ({
   key: key,
   videoId: videoId,
   playerState: -1,
-  ytInstance: null
+  ytInstance: null,
+  record: null,
+  recording: false
 })
 
 // h('i.fas.fa-x2.fa-play-circle')
@@ -15,11 +17,55 @@ const view = model => h('div.box', {key: model.key}, [
       h('figure#' + model.key + '.image.is-96x96')
     ]),
     h('div.media-content', [
-      h('button.button.is-large', {on: {click: () => togglePlay(model)}}, [
-        h('span.icon.is-medium', h('i.fas.fa-x2.' + getStateIcon(model)))
+      h('div.field.is-grouped', [
+        h('p.control', [
+          h('button.button.is-large', {on: {click: () => stepBackward(model)}}, [
+            h('span.icon.is-medium', h('i.fas.fa-x2.fa-step-backward'))
+          ])
+        ]),
+        h('p.control', [
+          h('button.button.is-large', {on: {click: () => fastBackward(model)}}, [
+            h('span.icon.is-medium', h('i.fas.fa-x2.fa-fast-backward'))
+          ])
+        ]),
+        h('p.control', [
+          h('button.button.is-large', {on: {click: () => togglePlay(model)}}, [
+            h('span.icon.is-medium', h('i.fas.fa-x2.' + getStateIcon(model)))
+          ])
+        ]),
+        h('p.control', [
+          h('button.button.is-large', {on: {click: () => stop(model)}}, [
+            h('span.icon.is-medium', h('i.fas.fa-x2.fa-stop'))
+          ])
+        ]),
+        h('p.control', [
+          h('button.button.is-large', {on: {click: () => rec(model)}}, [
+            h('span.icon.is-medium.has-text-danger', h('i.fas.fa-x2.fa-circle'))
+          ])
+        ]),
+        h('p.control', [
+          h('button.button.is-large', {on: {click: () => playLoop(model)}}, [
+            h('span.icon.is-medium', h('i.fas.fa-x2.fa-redo'))
+          ])
+        ]),
+        h('p.control', [
+          h('button.button.is-large', {on: {click: () => fastForward(model)}}, [
+            h('span.icon.is-medium', h('i.fas.fa-x2.fa-fast-forward'))
+          ])
+        ]),
+        h('p.control', [
+          h('button.button.is-large', {on: {click: () => stepForward(model)}}, [
+            h('span.icon.is-medium', h('i.fas.fa-x2.fa-step-forward'))
+          ])
+        ])
       ]),
-      h('button.button.is-large', {on: {click: () => stop(model)}}, [
-        h('span.icon.is-medium', h('i.fas.fa-x2.fa-stop'))
+      h('div.field', [
+        h('p.control', [
+          h('input.slider.is-fullwidth.is-large.is-warning', {
+            props: {step: 1, min: 0, max: 100, value: 50, type: 'range'},
+            on: {input: e => setVolume(model, e.target.value)}
+          })
+        ])
       ])
     ]),
     h('div.media-right', [
@@ -40,6 +86,49 @@ const getStateIcon = model => {
     case 1 : // – playing
       return 'fa-pause'
   }
+}
+
+const setVolume = (model, value) => {
+  model.ytInstance.setVolume(value)
+}
+
+const rec = model => {
+  if (!model.recording) {
+    model.recording = true
+    model.record = {
+      start: model.ytInstance.getCurrentTime(),
+      end: null
+    }
+  } else {
+    model.recording = false
+    model.record.end = model.ytInstance.getCurrentTime()
+  }
+  console.log('model.recording :', model.recording)
+}
+
+const playLoop = model => {
+  if (model.record.start < model.record.end) {
+    var loopDurationMili = (model.record.end - model.record.start) * 1000
+    console.log('loopDurationMili :', loopDurationMili)
+    setInterval(() => model.ytInstance.seekTo(model.record.start), loopDurationMili)
+    // TODO: a bit more work on this
+  }
+}
+
+const fastForward = model => {
+  model.ytInstance.seekTo(model.ytInstance.getCurrentTime() + 20, true)
+}
+
+const stepForward = model => {
+  model.ytInstance.seekTo(model.ytInstance.getCurrentTime() + 5, true)
+}
+
+const stepBackward = model => {
+  model.ytInstance.seekTo(model.ytInstance.getCurrentTime() - 5, true)
+}
+
+const fastBackward = model => {
+  model.ytInstance.seekTo(model.ytInstance.getCurrentTime() - 20, true)
 }
 
 const togglePlay = model => {
