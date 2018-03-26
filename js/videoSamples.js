9ce5e74ba58e6bd609c26125f4d1c34f54e6bca5
Clement Lepissier <clem.lepissier@gmail.com>
2018-03-26 17:09:41 +0200

    put videoSamples in separate file

---
+++ js/videoSamples.js
@@ -0,0 +1,26 @@
+module.exports = [
+  {
+    name: 'Digital Family | Vol 6',
+    videoId: 'UlPkNR83XOo'
+  },
+  {
+    name: '8 Hours of Whale Sounds...',
+    videoId: 'nDqP7kcr-sc'
+  },
+  {
+    name: 'Deleuze - Qu\'est-ce que l\'acte de création?',
+    videoId: '2OyuMJMrCRw'
+  },
+  {
+    name: 'Akhenaton Une Impression Acapella',
+    videoId: '-ETTCRznLSo'
+  },
+  {
+    name: 'Kacem Wapalek - Terrain Vague',
+    videoId: 'T7glIFtxAH0'
+  },
+  {
+    name: 'Gramatik - Muy Tranquilo',
+    videoId: 'mUnMOG8xKsg'
+  }
+]
