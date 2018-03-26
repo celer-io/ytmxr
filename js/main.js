b9cd0de41d962290777171e43e60bbb54a0536e8
Clement Lepissier <clem.lepissier@gmail.com>
2018-03-26 15:53:26 +0200

    add patch loop and externalize player instanciation in track component

--- js/main.js
+++ js/main.js
@@ -1,20 +1,27 @@
-/* global YT */
-var snabbdom = require('snabbdom')
+// /* global YT */
+const snabbdom = require('snabbdom')
 const R = require('ramda')
-var patch = snabbdom.init([ // Init patch function with chosen modules
+const patch = snabbdom.init([ // Init patch function with chosen modules
   require('snabbdom/modules/class').default, // makes it easy to toggle classes
   require('snabbdom/modules/props').default, // for setting properties on DOM elements
   require('snabbdom/modules/style').default, // handles styling on elements with support for animations
   require('snabbdom/modules/eventlisteners').default // attaches event listeners
 ])
-var h = require('snabbdom/h').default // helper function for creating vnodes
+const h = require('snabbdom/h').default // helper function for creating vnodes
+
+const MAX_PATCH_LOOP = 10
+const PATCH_INTERVAL = 500 // ms
+
 const Track = require('./track')
 
 var exampleVideos = ['UlPkNR83XOo', 'nDqP7kcr-sc', '2OyuMJMrCRw', 'XT6BMrbjvRs']
 const model = {
   tracks: [],
   nextId: 0,
-  newVideoId: ''
+  newVideoId: '',
+  dirty: true,
+  patchLoop: null,
+  patchLoopCount: 0
 }
 
 var vnode = h('section#app.hero.is-large.is-light', [
@@ -28,106 +35,90 @@ var vnode = h('section#app.hero.is-large.is-light', [
 patch(document.getElementById('app'), vnode)
 
 global.onYouTubeIframeAPIReady = () => {
-  view(model)
+  updateView(model)
 }
 
-document.addEventListener('discusting-non-functional-message', () => view(model))
+document.addEventListener('call-redraw', () => {
+  updateView(model)
+})
+
 document.addEventListener('delete-track', e => {
   model.tracks = R.reject(R.propEq('key', e.detail), model.tracks)
-  view(model)
+  updateView(model)
 })
 
 function addTrack (videoId) {
-  // TODO: make those updates with ramda ?
   model.tracks.push(Track.init(videoId, 'player' + model.nextId))
   model.nextId++
-
-  view(model)
-
-  var newTrack = R.last(model.tracks)
-  newTrack.ytInstance = new YT.Player(newTrack.key, {
-    height: '100',
-    width: '400',
-    videoId: videoId,
-    events: {
-      'onReady': onPlayerReady,
-      'onStateChange': onPlayerStateChange
-    },
-    playerVars: {
-      modestbranding: 0,
-      rel: 0
-    }
-  })
+  updateView(model)
 }
 
-const view = model => {
-  var newvNode = h('div#app', [
-    h('nav.navbar.is-light.is-fixed-top', [
-      h('div.container', [
-        h('div.navbar-brand', [
-          h('div.navbar-item.title.is-4', 'YTMXR')
-        ]),
-        h('div.navbar-menu.is-active', [
-          h('div.navbar-left', [
-            h('div.navbar-item', [
-              h('div.field.is-grouped', [
-                h('p.control.is-expanded', [
-                  h('input.input', {
-                    props: {type: 'text', placeholder: 'Enter a Youtube Id', value: model.newVideoId},
-                    on: {input: e => { model.newVideoId = e.target.value }} // TODO: make this update with ramda ?
-                  })
-                ]),
-                h('p.control', [
-                  h('button.button', {on: {click: () => addTrack(model.newVideoId)}}, 'Go')
-                ]),
-                h('p.control', [
-                  h('button.button', {on: {click: openRandom}}, 'Rand')
-                ])
+const view = model =>
+h('div#app', [
+  h('nav.navbar.is-light.is-fixed-top', [
+    h('div.container', [
+      h('div.navbar-brand', [
+        h('div.navbar-item.title.is-4', 'YTMXR')
+      ]),
+      h('div.navbar-menu.is-active', [
+        h('div.navbar-left', [
+          h('div.navbar-item', [
+            h('div.field.is-grouped', [
+              h('p.control.is-expanded', [
+                h('input.input', {
+                  props: {type: 'text', placeholder: 'Enter a Youtube Id', value: model.newVideoId},
+                  on: {input: e => { model.newVideoId = e.target.value }} // TODO: make this update with ramda ?
+                })
+              ]),
+              h('p.control', [
+                h('button.button', {on: {click: () => addTrack(model.newVideoId)}}, 'Go')
+              ]),
+              h('p.control', [
+                h('button.button', {on: {click: openRandom}}, 'Rand')
               ])
             ])
           ])
         ])
       ])
-    ]),
-    h('section.section', [
-      h('div.container', [
-        h('div.field.is-grouped',
-        R.map(
-          videoId => h('p.control', [
-            h('a.button.is-primary', {on: {click: () => addTrack(videoId)}}, videoId)
-          ]),
-          exampleVideos)
-        )
-      ])
-    ]),
-    h('section.section', [
-      h('div.container', R.map(t => Track.view(t), model.tracks))
     ])
-  ]
-)
-  patch(vnode, newvNode)
-  vnode = newvNode // Need statefull FRP shit ?
+  ]),
+  h('section.section', [
+    h('div.container', [
+      h('div.field.is-grouped',
+      R.map(
+        videoId => h('p.control', [
+          h('a.button.is-primary', {on: {click: () => addTrack(videoId)}}, videoId)
+        ]),
+        exampleVideos)
+      )
+    ])
+  ]),
+  h('section.section', [
+    h('div.container', R.map(t => Track.view(t), model.tracks))
+  ])
+])
+
+const updateView = model => {
+  if (model.patchLoop) {
+    model.dirty = true
+    model.patchLoopCount = 0
+  } else {
+    model.patchLoop = setInterval(() => {
+      if (model.dirty) {
+        var newvNode = view(model)
+        patch(vnode, newvNode)
+        vnode = newvNode
+        model.dirty = false
+      } else if (model.patchLoopCount++ >= MAX_PATCH_LOOP) {
+        model.patchLoopCount = 0
+        clearInterval(model.patchLoop)
+        model.patchLoop = null
+      }
+    }, PATCH_INTERVAL)
+  }
 }
 
 const openRandom = () => {
   var rand = Math.floor(Math.random() * exampleVideos.length)
   addTrack(exampleVideos[rand])
 }
-
-// ytapi handles
-function onPlayerReady (event) {
-  var track = R.find(R.propEq('key', event.target.a.id), model.tracks)
-  console.log('track ready :', track)
-  event.target.setVolume(50)
-  event.target.playVideo()
-}
-
-function onPlayerStateChange (event) {
-  var track = R.find(R.propEq('key', event.target.a.id), model.tracks)
-  track.playerState = event.data // Oulah grosse mutation !!!
-  view(model)
-}
-
-// function stopVideo () {
-//   players[0].stopVideo()
-// }
